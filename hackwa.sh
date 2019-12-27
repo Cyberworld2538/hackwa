19f3ee666f849fffdcf6dd1c734f3611582f3b8a
Cyberworld2538 <59283434+Cyberworld2538@users.noreply.github.com>
2019-12-27 18:49:17 +0700

    Add files via upload

---
+++ hackwa.sh
@@ -0,0 +1,52 @@
+
+clear
+cowsay -f eyes 'ARISANDI' | lolcat
+figlet -f slant "ARISANDI" | lolcat
+
+
+echo "Tools :HACK WHATSAPP" | lolcat
+echo "Author:ARISANDI" | lolcat
+echo "Team : HSD(HACKKER SCAN DARK)" | lolcat
+echo "====================" | lolcat
+
+echo "[~] Selamat mengggunakan Tool saya [~]"
+echo "[~] gunakan dengan bijak [~]"
+echo "[=] BY: :ARISANDI [=]"
+
+echo "_____________________________________________________________"
+echo "Tools    : Kumpulan ASW Tools $white                         " |lolcat
+echo "Creadby  : ARISANDI $white                                      " |lolcat
+echo "Contact  : 081336501311   $white                             " |lolcat
+echo "_____________________________________________________________"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $red"[#]> (Ctrl + C ) "
+echo $cyan"[#]> Thanks"
+sleep 1
+echo ""
+echo $white"[#]> see you gayn :)"
+sleep 1
+}
+
+read -p "Masukkan Nomer kamu (62*****) :" pilih
+read -p "Masukkan Nomer target (62*****) :" pilih
+sleep 1
+echo "login(62*****)"
+sleep 1
+echo "target(62*****)"
+sleep 2
+echo "Nomer Target Di Temukan"
+sleep 3
+
+echo "[+]Syncing Target..."
+sleep 5
+echo "[+]DONE..."
+sleep 5
+echo "Tunggu 5-10 Menit Untuk Mendapatkan Code."
+sleep 9
+echo "code nya =>965427"
